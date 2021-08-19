dc0b1ad929e1ed0f1052aed5258dd3c59c50b01d
annurdien <ajwadax@gmail.com>
2021-08-19 20:40:24 +0700

    TESTING

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:girlscode_example/widgets/splash.dart';
 import 'package:girlscode_example/widgets/theme.dart';
 
-import 'widgets/skills.dart';
-
 void main() {
   runApp(MyApp());
 }
